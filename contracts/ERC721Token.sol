eeb5d890a2b3484acb6620b64cb96a8dd8205dfc
tungla <tungleanh.ptit@gmail.com>
2022-12-27 21:44:08 +0700

    done

--- contracts/ERC721Token.sol
+++ contracts/ERC721Token.sol
@@ -13,22 +13,27 @@ contract ERC721Token is ERC721Enumerable, Ownable {
     // Mapping from token ID to owner address
     mapping(uint256 => address) private _owners;
 
+    mapping(uint256 => string[]) public _dynamicUri;
+
     constructor(string memory _name, string memory _symbol)
         ERC721(_name, _symbol)
     {}
 
-    function mint(address _owner, uint256 _mintAmount)
+    function mint(address _owner, uint256 _mintAmount, string[] calldata _uri)
         external
         onlyOwner
-        returns (uint256)
     {
         uint256 supply = totalSupply();
 
         for (uint256 i = 1; i <= _mintAmount; ++i) {
             _owners[supply + i] = _owner;
             _safeMint(_owner, supply + i);
+            _dynamicUri[supply + i].push(_uri[i]);
         }
-        return supply + 1;
+    }
+
+    function updateURI(uint256 _tokenId, string calldata _uri) onlyOwner external {
+        _dynamicUri[_tokenId].push(_uri);
     }
 
     /**
@@ -48,17 +53,16 @@ contract ERC721Token is ERC721Enumerable, Ownable {
             address owner = _owners[_tokenId];
             require(_owner == owner, "ERC721Burnable: caller is not owner");
             _burn(_tokenId);
+            delete _dynamicUri[_tokenId];
         }
     }
 
-    function _baseURI() internal view virtual override returns (string memory) {
-        return "";
+    function tokenURI(uint256 _tokenId) public view virtual override returns (string memory) {
+        _requireMinted(_tokenId);
+        string[] memory uris = _dynamicUri[_tokenId];
+        return uris[uris.length-1];
     }
-
-    /* 
-    Dynamic NFT:
-    - Attributes in metadata:
-        + 
-    - Paper relate to nft:
-     */
 }
+
+// access control
+// dynamic split or combine?
